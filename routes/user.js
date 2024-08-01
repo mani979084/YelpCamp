7022d53afe8b1c9fe30602ef2a119f6e437597e4
Mani <mani979084@gmail.com>
2024-08-01 23:34:50 +0530

    redirect fix

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const catchAsync = require('../error/catchAsync');
-const users = require('../controllers/user');
+const passport = require("passport");
+const catchAsync = require("../error/catchAsync");
+const users = require("../controllers/user");
 
-router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.registerUser))
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.registerUser));
 
+router.get("/loginFail", (req, res) => {
+  res.json({ error: "Login failed" });
+});
 
-router.get('/loginFail', (req, res) => {
-    res.json({ error: 'Login failed' })
-})
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", { failureRedirect: "/api/loginFail" }),
+    users.loginUser
+  );
 
-router.route('/login')
-    .get(users.renderLogin)
-    .post(passport.authenticate('local', { failureRedirect: '/loginFail' }),
-        users.loginUser)
-
-router.get('/logout', users.logoutUser)
+router.get("/logout", users.logoutUser);
 
 module.exports = router;
